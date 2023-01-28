1f277bf1a9340a3424c99f37361fe2f6144030de
Pjeter Zimaj <pz3920@rit.edu>
2023-01-28 14:35:53 +0100

    Adding methods for uploading

--- frontend/pinrit-frontend/src/scripts/web3_services.js
+++ frontend/pinrit-frontend/src/scripts/web3_services.js
@@ -3,6 +3,11 @@ import myJson from '../../../../Marketplace.json' assert {type: 'json'};
 
 let contract ="0xE5E559cd3C67d02840257518e81A16324C07415A"
 
+let name = document.getElementById("name")
+let description = document.getElementById("description")
+let price = document.getElementById("price")
+let formParams = {name, description, price}
+
 //loading web3
 async function loadWeb3() {
     if (window.ethereum) {
@@ -62,6 +67,75 @@ async function printAccount() {
     accountAddress.appendChild(textNode);
 }
 
+function updateFormParams(parName, parDesc, parPrice) {
+    formParams.name = parName
+    formParams.description = parDesc
+    formParams.price = parPrice
+}
+
+async function OnChangeFile(e) {
+    var file = e.target.files[0];
+    //check for file extension
+    try {
+        //upload the file to IPFS
+        const response = await uploadFileToIPFS(file);
+        if(response.success === true) {
+            console.log("Uploaded image to Pinata: ", response.pinataURL)
+            setFileURL(response.pinataURL);
+        }
+    }
+    catch(e) {
+        console.log("Error during file upload", e);
+    }
+}
+
+async function uploadMetadataToIPFS() {
+    const {name, description, price} = formParams;
+    //Make sure that none of the fields are empty
+    if( !name || !description || !price || !fileURL)
+        return;
+
+    const nftJSON = {
+        name, description, price, image: fileURL
+    }
+
+    try {
+        //upload the metadata JSON to IPFS
+        const response = await uploadJSONToIPFS(nftJSON);
+        if(response.success === true){
+            console.log("Uploaded JSON to Pinata: ", response)
+            return response.pinataURL;
+        }
+    }
+    catch(e) {
+        console.log("error uploading JSON metadata:", e)
+    }
+}
+
+async function listNFT(e) {
+    e.preventDefault();
+
+    //Upload data to IPFS
+    try {
+        const metadataURL = await uploadMetadataToIPFS();
+        //massage the params to be sent to the create NFT request
+        const price = ethers.utils.parseUnits(formParams.price, 'ether')
+        let listingPrice = await window.contract.getListPrice()
+        listingPrice = listingPrice.toString()
+
+        //actually create the NFT
+        let transaction = await window.contract.createToken(metadataURL, price, { value: listingPrice })
+        await transaction.wait()
+
+        alert("Successfully listed your NFT!");
+        updateMessage("");
+        updateFormParams('', '', '');
+        window.location.replace("/")
+    }
+    catch(e) {
+        alert( "Upload error"+e )
+    }
+}
 
 // // printing test data
 // async function mint() {
