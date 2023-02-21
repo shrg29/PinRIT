bdb48caa26ef7165496c0e82bc0003330f2b9758
Matej <mp2272@g.rit.edu>
2023-02-21 20:32:07 +0100

    .

--- frontend/pinrit-frontend/src/scripts/web3_services.js
+++ frontend/pinrit-frontend/src/scripts/web3_services.js
@@ -214,82 +214,102 @@ async function uploadArtwork(e) {
         console.log("Upload error" + e)
     }
 }
+const buyButton = document.getElementById('buyButton')
+buyButton.addEventListener('click', buyArtwork)
+
+async function buyArtwork(resp) {
+    let data
+    try {
+        console.log(resp)
+        let info = await getInfo() //vraca dosl ovak cijeli info
+
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+const price = ethers.utils.parseUnits(resp.price, 'ether')
+
+
+        console.log(price)
+            let transaction = await window.contract.methods.buyNFT(resp.tokenId).send({ from: account, gas: 3000000, value: price }, function (err, res) { })
+            await transaction.wait()
+        } catch (error) {
+            console.log("Buy error ", error)
+        }
+    }
 
 function updateFetched(isFetched) {
-    fetched = isFetched
-}
+        fetched = isFetched
+    }
 
-//this actually gets all images of the NFTs
-async function getAllNFTs() {
-    
-    let listOfUrls = ""
-    //we are calling the contract method which returns the array of all nfts (info about owners, price and id)
-    let transaction = await contract.methods.getAllNFTs().call().then(function (array) {
-        let newTransaction = array
-       // console.log("info s ugovora " + array)
-        
-        //go through everything from the array and based on the ID get the generated url
-        return Promise.all(newTransaction.map(async i => {
-            const tokenURI = await window.contract.methods.tokenURI(i.tokenID).call()
-            //console.log(tokenURI)
-            return tokenURI
-        })).then(results => {
-            listOfUrls = results
-           // console.log("cijeli array " + results)
-            return results
-            
+    //this actually gets all images of the NFTs
+    async function getAllNFTs() {
+
+        let listOfUrls = ""
+        //we are calling the contract method which returns the array of all nfts (info about owners, price and id)
+        let transaction = await contract.methods.getAllNFTs().call().then(function (array) {
+            let newTransaction = array
+            // console.log("info s ugovora " + array)
+
+            //go through everything from the array and based on the ID get the generated url
+            return Promise.all(newTransaction.map(async i => {
+                const tokenURI = await window.contract.methods.tokenURI(i.tokenID).call()
+                //console.log(tokenURI)
+                return tokenURI
+            })).then(results => {
+                listOfUrls = results
+                // console.log("cijeli array " + results)
+                return results
+            })
         })
-    })
-    return transaction
+        return transaction
 
-}
+    }
 
-//vraca cijeli info i stavlja ga u array
-async function getInfo() {
-    let listOfUrls = ""
-    //we are calling the contract method which returns the array of all nfts (info about owner, price and id)
-    let transaction = await contract.methods.getAllNFTs().call().then(function (array) {
-        let newTransaction = array
+    //vraca cijeli info i stavlja ga u array
+    async function getInfo() {
+        let listOfUrls = ""
+        //we are calling the contract method which returns the array of all nfts (info about owner, price and id)
+        let transaction = await contract.methods.getAllNFTs().call().then(function (array) {
+            let newTransaction = array
 
-        //go through everything from the array and based on the ID get info 
-        return Promise.all(newTransaction.map(async i => {
-            const tokenURI = await window.contract.methods.tokenURI(i.tokenID).call()
-            let meta = await axios.get(tokenURI, {
-                headers: {
-                    'Accept': 'text/plain'
+            //go through everything from the array and based on the ID get info 
+            return Promise.all(newTransaction.map(async i => {
+                const tokenURI = await window.contract.methods.tokenURI(i.tokenID).call()
+                let meta = await axios.get(tokenURI, {
+                    headers: {
+                        'Accept': 'text/plain'
+                    }
+                });
+                meta = meta.data;
+
+                let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+                let item = {
+                    tokenId: i.tokenID,
+                    price,
+                    seller: i.seller,
+                    owner: i.owner,
+                    image: meta.image,
+                    name: meta.name,
+                    description: meta.description,
                 }
-            });
-            meta = meta.data;
+                //console.log(item)
+                return item;
 
-            let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-            let item = {
-                tokenId: i.tokenID,
-                price,
-                seller: i.seller,
-                owner: i.owner,
-                image: meta.image,
-                name: meta.name,
-                description: meta.description,
-            }
-            //console.log(item)
-            return item;
-
-            return tokenURI
-        })).then(results => {
-            listOfUrls = results
-            // console.log(listOfUrls)
-            return results
+                return tokenURI
+            })).then(results => {
+                listOfUrls = results
+                // console.log(listOfUrls)
+                return results
+            })
         })
-    })
-    return transaction
-}
+        return transaction
+    }
 
-function appendInfo(resp) {
-    console.log(resp)
-    const el = document.getElementById("info");
-    let textNode = document.createTextNode(JSON.stringify(resp));
-    el.innerHTML = 
-    `
+    function appendInfo(resp) {
+        console.log(resp)
+        const el = document.getElementById("info");
+        let textNode = document.createTextNode(JSON.stringify(resp));
+        el.innerHTML =
+            `
     <div class="info-form">
     <div class="circle-el">
     <img class="circle" src= ${resp.image} width=115 height=115/>
@@ -302,36 +322,36 @@ function appendInfo(resp) {
     <p> Price: ${resp.price} ETH</p>
     </div>
     `;
-}
-
-//loads at page rendering
-async function load() {
-
-    await printAccount();
-    await loadWeb3();
-    window.contract = await loadContract();
-    await getAllAccounts();
-    //await getAllNFTs();
-    await getInfo();
-
-    if (!fetched) {
-        let info = await getInfo() //vraca dosl ovak cijeli info
-        const root = document.getElementById("nftData");
-        for (const response of info) {
-            const img = document.createElement("img");
-            img.src = response.image;
-            img.id = response.image;
-            img.addEventListener('click', function handleClick(event) {
-            appendInfo(response)
-            });
-            root.appendChild(img);
-        }
-        updateFetched(true)
     }
 
-}
+    //loads at page rendering
+    async function load() {
 
-load();
+        await printAccount();
+        await loadWeb3();
+        window.contract = await loadContract();
+        await getAllAccounts();
+        //await getAllNFTs();
+        await getInfo();
+
+        if (!fetched) {
+            let info = await getInfo() //vraca dosl ovak cijeli info
+            const root = document.getElementById("nftData");
+            for (const response of info) {
+                const img = document.createElement("img");
+                img.src = response.image;
+                img.id = response.image;
+                img.addEventListener('click', function handleClick(event) {
+                    appendInfo(response)
+                });
+                root.appendChild(img);
+            }
+            updateFetched(true)
+        }
+
+    }
+
+    load();
 
 
 
